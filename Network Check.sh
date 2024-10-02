86b68b1a6a6cf05c6f47ac117e39ebee9c5b206f
Ronnee Sears <140205213+RSears0314@users.noreply.github.com>
2024-10-02 17:24:13 -0500

    Update Network Check.sh

--- Network Check.sh
+++ Network Check.sh
@@ -1,7 +1,16 @@
-touch network_check_data.txt
-echo "Commencing the network and system check..."
-ping 8.8.8.8 -c 3 -w 5 > network_check_data.txt
+#NOTES
 
+#This script checks basic network connectivity, storing the recieved data in a file it creates, then searching those files for the data it needs to return the diagnosis
+
+#The "grep" command (in this use case) is searching a file for a specfic script;
+#Syntax: if grep -q "STRING" "FILE TO SEARCH". The -q flag allows it to run silently
+
+
+touch network_check_data.txt #This creates a data document to store the computers network data during the running of the script
+echo "Commencing the network and system check..."
+ping 8.8.8.8 -c 3 -w 5 > network_check_data.txt #Pinging google to make sure the network works, then storing the data in the network_check_data.txtx file
+
+#############################################################################
 if grep -q " 0% packet loss" network_check_data.txt; then
         echo "Your network connection is stable. Commencing interface check..."
 elif grep -q "100% packet loss" network_check_data.txt; then
@@ -10,23 +19,26 @@ elif grep -q "100% packet loss" network_check_data.txt; then
 else echo "Test inconclusive, commencing network interface check..."
 fi
 
-sleep 2s
+sleep 2s #Pauses the shell to make the script more readable for the user
+#############################################################################
 
-ip link show > network_check_data.txt
+ip link show > network_check_data.txt #Checking the status of the network links, then storing the data in the network_check_data.txtx file
 
+#############################################################################
 if grep -q "LOOPBACK,UP" network_check_data.txt; then
         echo "Your Loopback interface is working properly"
 else grep -q "state DOWN" network_check_data.txt;
-        echo "Your Loopback or internet interface appears to be having issues..."
+        echo "Your Loopback or an internet interface appears to be having issues..."
 fi
 
 sleep 1s
+#############################################################################
 
-uptime_String=$'\nNetwork test complete, listing system uptime...'
-echo "$uptime_String"
+uptime_String=$'\nNetwork test complete, listing system uptime...' #The use of single quotations here is important
+echo "$uptime_String" #The use of double quotations here is important
 
 sleep 2s
 
-uptime -p
+uptime -p #Uptime shows the device uptime, and the p flag shows it in a 'pretty' way
 
-rm network_check_data.txt
+rm network_check_data.txt #Deletes the data file the script creates
